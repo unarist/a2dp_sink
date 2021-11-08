fcd7f9420681f173c95966856462c7d72e768f79
unarist <m.unarist@gmail.com>
2021-11-08 22:08:57 +0900

    impl watch

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+
+use std::{error::Error, io::stdin};
+
+use windows::{Devices::Enumeration::DeviceInformation, Foundation::TypedEventHandler, Media::Audio::AudioPlaybackConnection};
+
 fn main() {
-    println!("Hello, world!");
+    match run() {
+        Ok(_) => (),
+        Err(err) => println!("Error: {}", err)
+    }
 }
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let selector = AudioPlaybackConnection::GetDeviceSelector()?;
+    let device_watcher = DeviceInformation::CreateWatcherAqsFilter(selector)?;
+    
+    device_watcher.Added(TypedEventHandler::new(|_sender, device_info: &Option<DeviceInformation>| {
+        println!("Added: {0}", device_info.as_ref().unwrap().Name()?);
+        Ok(())
+    })).unwrap();
+    device_watcher.Start()?;
+    println!("Waiting for connection. Press enter to exit.");
+
+    let mut input = String::new();
+    stdin().read_line(&mut input)?;
+    Ok(())
+}
+
